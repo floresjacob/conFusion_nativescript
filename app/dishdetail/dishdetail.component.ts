63c7612e99e807f103bc331cb965b7ac21dcc241
Jacob Flores <jacob.e.flores562@gmail.com>
2017-12-17 20:47:47 -0800

    Customize Android

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -10,6 +10,12 @@ import { Toasty } from 'nativescript-toasty';
 import { CommentComponent } from '../comment/comment.component';
 import * as dialogs from 'ui/dialogs';
 import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/modal-dialog';
+import { Page } from "ui/page";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Color } from 'color';
+import * as enums from "ui/enums";
 import 'rxjs/add/operator/switchMap';
 
 @Component({
@@ -26,8 +32,14 @@ export class DishdetailComponent implements OnInit {
   avgstars: string;
   numcomments: number;
   favorite: boolean = false;
+  showComments: boolean = false;
+
+  cardImage: View;
+  commentList: View;
+  cardLayout: View;
 
   constructor(private dishservice: DishService,
+    private page: Page,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
     private favoriteservice: FavoriteService,
@@ -50,6 +62,97 @@ export class DishdetailComponent implements OnInit {
           this.avgstars = (total/this.numcomments).toFixed(2);
         },
         errmess => { this.dish = null; this.errMess = <any>errmess; });
+  }
+
+  onSwipe(args: SwipeGestureEventData) {
+
+    if (this.dish) {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (args.direction === SwipeDirection.up && !this.showComments ) {
+        this.animateUp();
+      }
+      else if (args.direction === SwipeDirection.down && this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+    }
+
+  }
+
+  showAndHideComments() {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (!this.showComments ) {
+        this.animateUp();
+      }
+      else if (this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+  }
+
+  animateUp() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 0 },
+        translate: { x: 0, y: -200 },
+        opacity: 0,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffc107"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+      this.showComments = true;
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  }
+
+  animateDown() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 1 },
+        translate: { x: 0, y: 0 },
+        opacity: 1,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffffff"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
   }
 
   goBack(): void {
--- platforms/android/build/intermediates/assets/F0F1/debug/app/shared/baseurl.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseURL = "http://192.168.1.5:3000/";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFzZXVybC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImJhc2V1cmwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBYSxRQUFBLE9BQU8sR0FBRywwQkFBMEIsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBiYXNlVVJMID0gXCJodHRwOi8vMTkyLjE2OC4xLjU6MzAwMC9cIjtcbiJdfQ==
