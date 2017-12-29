6110279f38b38e78e8f42dd6c72e250e9b39dd3d
Jacob Flores <jacob.e.flores562@gmail.com>
2017-12-29 05:42:41 -0800

    Camera

--- app/app.module.ts
+++ app/app.module.ts
@@ -19,6 +19,7 @@ import { FavoritesComponent } from "./favorites/favorites.component";
 import { ReservationComponent } from './reservation/reservation.component';
 import { ReservationModalComponent } from "./reservationmodal/reservationmodal.component";
 import { CommentComponent } from "./comment/comment.component";
+import { UserAuthComponent } from "./userauth/userauth.component";
 
 import { PromotionService } from "./services/promotion.service";
 import { LeaderService } from "./services/leader.service";
@@ -62,7 +63,8 @@ import { baseURL } from "./shared/baseurl";
         FavoritesComponent,
         ReservationComponent,
         ReservationModalComponent,
-        CommentComponent
+        CommentComponent,
+        UserAuthComponent
     ],
     providers: [
         {provide: 'BaseURL', useValue: baseURL},
